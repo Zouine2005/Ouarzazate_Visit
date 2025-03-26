e6dff5db34339d4360dc76b89e86d219893afe82
Zouine2005 <zouinemohamade@gmail.com>
2025-03-26 02:34:42 +0000

    add new route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LanguageController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Landing');
 });
+
+Route::post('/change-language', [LanguageController::class, 'change'])->name('language.change');
+
